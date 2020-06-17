b356730210bf18a586b21ac8b60481ebe5b24085
sarumathiramasamy <43061815+sarumathiramasamy@users.noreply.github.com>
2020-06-17 11:40:34 +0800

    Updates

---
+++ precache-manifest.140245510cdc67d86e086ca8dcd8bada.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3897c798915cea0ca1a1e342b28cce13",
+    "url": "/ParallaxEffect/index.html"
+  },
+  {
+    "revision": "59ee91961fcc9d54d971",
+    "url": "/ParallaxEffect/static/css/main.709bf579.chunk.css"
+  },
+  {
+    "revision": "ca6e2bed4bd156215006",
+    "url": "/ParallaxEffect/static/js/2.e0ffb21a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ParallaxEffect/static/js/2.e0ffb21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ee91961fcc9d54d971",
+    "url": "/ParallaxEffect/static/js/main.1f973f03.chunk.js"
+  },
+  {
+    "revision": "1036ce606eb3748678f6",
+    "url": "/ParallaxEffect/static/js/runtime-main.aa767b3b.js"
+  }
+]);
